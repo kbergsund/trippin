4f23c137eeb93ea9bad63d168e90e08b94a1501a
Kyra Bergsund <k.bergsund@gmail.com>
2021-11-12 17:51:29 -0800

    Implement trip category dropdown functionality, dynamic user info load

--- src/scripts.js
+++ src/scripts.js
@@ -4,6 +4,8 @@ import './images/user.svg';
 import { fetchData } from './fetch';
 import TravelerRepo from './TravelerRepo'
 
+let travelerRepo;
+
 const retrieveData = () => {
   Promise.all(
     [fetchData('travelers'), fetchData('trips'), fetchData('destinations')]
@@ -12,14 +14,83 @@ const retrieveData = () => {
       Object.values(data[0])[0], 
       Object.values(data[1]).flat(), 
       Object.values(data[2]).flat());
+    renderDOM();
   })
+  // .then(() => renderDOM())
 }
 
 const buildTravelerRepo = (travelerData, tripData, destinationData) => {
-  const travelerRepo = 
+  travelerRepo = 
     new TravelerRepo(travelerData, tripData, destinationData);
   travelerRepo.buildTravelers();
-  console.log(travelerRepo);
+}
+
+const getRandomIndex = (array) => {
+  return Math.floor(Math.random() * array.length + 1);
+};
+
+const userDropdown = document.querySelector('#dropdownContent');
+const currentTrip = document.querySelector('#currentTrip');
+const tripCategories = document.querySelector('#tripCategories')
+const upcoming = document.querySelector('#upcoming');
+const past = document.querySelector('#past');
+const pending = document.querySelector('#pending');
+
+tripCategories.addEventListener('change', toggleTripView);
+
+const renderDOM = () => {
+  const randomID = getRandomIndex(travelerRepo.allTravelers);
+  travelerRepo.retrieveTraveler(28);
+  const traveler = travelerRepo.currentTraveler
+  console.log(travelerRepo.currentTraveler);
+  displayUserInfo(traveler);
+  displayCurrentTrip(traveler);
+  displayTrips(traveler);
+  toggleTripView();
+}
+
+const displayUserInfo = (traveler) => {
+  userDropdown.innerHTML = `
+  <p>${traveler.name}</p>
+  <p>I am a: ${traveler.travelerType}</p>
+  <p>2021 Total Spend: $${traveler.myTrips.calculateTotalCostThisYear()}<p>
+  `
+}
+
+// Not Tested!
+const displayCurrentTrip = (traveler) => {
+  console.log(traveler.myTrips.categorizedTrips.present);
+  if (traveler.myTrips.categorizedTrips.present.length > 0) {
+    const currentDestination = traveler.myTrips.categorizedTrips.present[0].destinationID
+    currentTrip.childNodes[3].innerHTML = `
+    <h3>${currentDestination}<h3>
+    `
+    // <p> element for start date & duration?
+  } 
+}
+const displayTrips = (traveler) => {
+
+}
+
+function toggleTripView(traveler) {
+  console.log(tripCategories.value)
+  switch (tripCategories.value) {
+  case 'upcoming':
+    upcoming.style.display = 'block';
+    past.style.display = 'none';
+    pending.style.display = 'none';
+    break;
+  case 'past':
+    upcoming.style.display = 'none';
+    past.style.display = 'block';
+    pending.style.display = 'none';
+    break;
+  case 'pending':
+    upcoming.style.display = 'none';
+    past.style.display = 'none';
+    pending.style.display = 'block';
+    break;
+  }
 }
 
 retrieveData();
