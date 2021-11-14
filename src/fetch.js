7ab4c3a97daa48be11a2ce7105fd5c07629459d5
Kyra Bergsund <k.bergsund@gmail.com>
2021-11-14 10:08:56 -0800

    Implement add trip in full, display new trip in pending

--- src/fetch.js
+++ src/fetch.js
@@ -1,10 +1,41 @@
+import { retrieveData } from './scripts';
+
 const fetchData = (url) => {
   return fetch(`http://localhost:3001/api/v1/${url}`)
     .then(response => response.json())
-    .catch(() => 
-      document.querySelector('h1').innerText = 'Error loading! Try again.');
+    .catch(error => showError(error)) 
+}
+
+const postData = (tripDetails) => {
+  return fetch('http://localhost:3001/api/v1/trips', {
+    method: 'POST',
+    body: JSON.stringify(tripDetails),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => checkForErrors(response))
+    .then(() => retrieveData())
+    .catch(error => showError(error));
+}
+
+const checkForErrors = (response) => {
+  if (!response.ok) {
+    throw new Error('Something went wrong. Try again!')
+  }
+  return response.json()
+}
+
+const showError = (error) => {
+  const errorField = document.querySelector('h1')
+  if (error.message === 'Failed to fetch') {
+    errorField.innerText = 'Error loading! Have you started the local server?';
+  } else {
+    errorField.innerText = `${error.message}`
+  }
 }
 
 export {
-  fetchData
+  fetchData,
+  postData
 }
